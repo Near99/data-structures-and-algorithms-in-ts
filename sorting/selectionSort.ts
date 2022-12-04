764d5fd773bd512b6b82ba588c8b3dd786d60d6d
Near99 <wkx0823@gmail.com>
2022-12-04 14:10:09 +0800

    selection sort in place

--- sorting/selectionSort.ts
+++ sorting/selectionSort.ts
@@ -1,25 +1,51 @@
 const inputArr = [5, 3, 10, 7, 1, 6, 9, 4, 8, 2];
 
-const findSmallest = (arr: Array<number>) => {
-    let idx = 0,
-        smallest = arr[0];
-    arr.forEach((number, index) => {
-        if (number < smallest) {
-            (smallest = number), (idx = index);
+/**
+ * Return sorted new array.
+ * Space: O(n)
+ */
+const textBookEasyToRead = () => {
+    const findSmallest = (arr: Array<number>) => {
+        let idx = 0,
+            smallest = arr[0];
+        arr.forEach((number, index) => {
+            if (number < smallest) {
+                (smallest = number), (idx = index);
+            }
+        });
+        return idx;
+    };
+
+    const selectionSort = (arr: Array<number>) => {
+        const sortedArr: Array<number> = [];
+        let i = 0,
+            j = arr.length;
+        for (; i < j; i++) {
+            const idx = findSmallest(arr);
+            sortedArr.push(arr.splice(idx, 1)[0]);
         }
-    });
-    return idx;
+        return sortedArr;
+    };
+
+    console.log(selectionSort(inputArr));
 };
 
-const selectionSrot = (arr: Array<number>) => {
-    const sortedArr: Array<number> = [];
-    let i = 0,
-        j = arr.length;
-    for (; i < j; i++) {
-        const idx = findSmallest(arr);
-        sortedArr.push(arr.splice(idx, 1)[0]);
-    }
-    return sortedArr;
+/**
+ * Sort array in place.
+ * Space: O(1)
+ */
+const sortInPlace = () => {
+    const selectionSort = (arr: Array<number>) => {
+        for (let i = 0; i < arr.length; i++) {
+            let s = i;
+            for (let j = s + 1; j < arr.length; j++) {
+                if (arr[j] < arr[s]) s = j;
+            }
+            const temp = arr[i];
+            arr[i] = arr[s];
+            arr[s] = temp;
+        }
+    };
+    selectionSort(inputArr);
+    console.log(inputArr);
 };
-
-console.log(selectionSrot(inputArr));
