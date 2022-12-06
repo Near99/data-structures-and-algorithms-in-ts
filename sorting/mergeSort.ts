804da4e6cdec512fe1a621676c72da0907a31841
Charles <wkx0823@gmail.com>
2022-12-06 16:18:59 +0800

    Add files via upload

---
+++ sorting/mergeSort.ts
@@ -0,0 +1,40 @@
+const inputArrM = [5, 3, 10, 7, 1, 6, 9, 4, 8, 2];
+
+const merge_ = (arr: Array<number>, low: number, middle: number, high: number) => {
+    let i = 0;
+    const bufferOne: Array<number> = [],
+        bufferTwo: Array<number> = [];
+    for (i = low; i <= middle; i++) bufferOne.push(arr[i]);
+    for (i = middle + 1; i <= high; i++) bufferTwo.push(arr[i]);
+    let bufferOneIndex = 0,
+        bufferTwoIndex = 0;
+    i = low;
+    while (bufferOneIndex < bufferOne.length && bufferTwoIndex < bufferTwo.length) {
+        if (bufferOne[bufferOneIndex] > bufferTwo[bufferTwoIndex]) {
+            arr[i++] = bufferTwo[bufferTwoIndex++];
+        } else {
+            arr[i++] = bufferOne[bufferOneIndex++];
+        }
+    }
+    while (bufferOneIndex < bufferOne.length) {
+        arr[i++] = bufferOne[bufferOneIndex++];
+    }
+    while (bufferTwoIndex < bufferTwo.length) {
+        arr[i++] = bufferTwo[bufferTwoIndex++];
+    }
+};
+
+const mergeSort_ = (arr: Array<number>, low: number, high: number) => {
+    if (low >= high) return;
+    const middle = Math.floor((low + high) / 2);
+    mergeSort_(arr, low, middle);
+    mergeSort_(arr, middle + 1, high);
+    merge_(arr, low, middle, high);
+};
+
+const mergeSort = (arr: Array<number>) => {
+    mergeSort_(arr, 0, arr.length - 1);
+};
+
+mergeSort(inputArrM);
+console.log(inputArrM);
